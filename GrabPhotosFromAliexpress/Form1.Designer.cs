2593366d8e5c3874760eeb2972199c874f7410e9
anyappscompany <56433722+anyappscompany@users.noreply.github.com>
2020-01-02 16:54:09 +0200

    Add files via upload

---
+++ GrabPhotosFromAliexpress/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace GrabPhotosFromAliexpress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(515, 218);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(528, 243);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Страницы откуда воровать картинки";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(515, 75);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 305);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(527, 100);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Статистика";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(550, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.helpToolStripMenuItem.Text = "Справка";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.aboutToolStripMenuItem.Text = "О программе";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(94, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Остановить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(248, 281);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(21, 13);
+            this.resultLabel.TabIndex = 10;
+            this.resultLabel.Text = "0%";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Выполнено:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 417);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Grab Photos from Aliexpress.com";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+
+    }
+}
+
---
+++ GrabPhotosFromAliexpress/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace GrabPhotosFromAliexpress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            if (backgroundWorker1.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+                backgroundWorker1.RunWorkerAsync();
+            }
+
+            
+        }
+
+        private bool grabPhotos(string url)
+        {
+            List<string> imagesFromPage = new List<string>();
+
+            string html = getPage(url);
+            //webBrowser1.DocumentText = html;
+            //string pattern = @"(?<=<img .*?src\s*=\s*"")[^""]+(?="".*?>)";
+            string pattern = @"<div class=""ui-tab-body"">(.*)<div id=""feedback"" class=""ui-tab-pane"">";
+            MatchCollection match = Regex.Matches(html, pattern, RegexOptions.Multiline);
+            Regex exp = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            match = exp.Matches(html);
+            if (match.Count <= 0)
+                return false;
+            html = match[0].Value;
+
+            if (match.Count >0 ) //найдено тело карточки товара
+            {
+                string patternImgs = @"(?<=<img .*?src\s*=\s*"")[^""]+(?="".*?>)";
+                MatchCollection matchImgs = Regex.Matches(html, pattern, RegexOptions.Multiline);
+                Regex expImgs = new Regex(patternImgs, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                matchImgs = expImgs.Matches(html);
+
+                for (int i = 0; i < matchImgs.Count; i++ )
+                {                     
+                    imagesFromPage.Add(matchImgs[i].Value);
+                }
+            }
+
+            if(imagesFromPage.Count > 0)
+            {
+                if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "images/"))
+                {
+                    Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "images/");
+                }
+
+                string foldName = "";
+                string[] split = url.Split(new Char[] { '/' });
+                foldName = split[split.Count() -1].Replace(".html", "");
+
+                if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "images/" + foldName))
+                {
+                    Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "images/" + foldName);
+                }
+
+                foreach(string imaga in imagesFromPage)
+                {
+                    //MessageBox.Show(imaga);
+                    string [] splitImaga = imaga.Split(new Char [] {'/'});
+                    string kartinka = splitImaga[splitImaga.Count() - 1];
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile(imaga, AppDomain.CurrentDomain.BaseDirectory + "images/" + foldName + "/" + kartinka);
+                }
+                return true;
+            }
+           
+
+            return false;
+        }
+
+        private string getPage(string page)
+        {
+            WebRequest reqGET = WebRequest.Create(page);
+            WebResponse resp = reqGET.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string html = sr.ReadToEnd();
+            return html;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+            {
+                // Cancel the asynchronous operation.
+                backgroundWorker1.CancelAsync();
+            }
+        }
+        public delegate void MethodInvoker();
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+                BackgroundWorker worker = sender as BackgroundWorker;
+
+                int totalUrl = textBox1.Lines.Length;
+
+                if (totalUrl > 0)
+                {
+                    String[] s = textBox1.Text.Split(new String[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                    int i = 1;
+                    foreach (string url in s)
+                    {
+                        if (worker.CancellationPending == true)
+                        {
+                            e.Cancel = true;
+                            break;
+                        }
+                        else
+                        {
+                            this.Invoke((MethodInvoker)delegate()
+    {
+        if (richTextBox1.Lines.Length > 50)
+            richTextBox1.Clear();
+    });
+                            try
+                            {
+                            if (grabPhotos(url))
+                            {
+                                this.Invoke((MethodInvoker)delegate()
+    {
+        richTextBox1.AppendText(Environment.NewLine + "+ " + url);
+    });
+                            }
+                            else
+                            {
+                                this.Invoke((MethodInvoker)delegate()
+    {
+        richTextBox1.AppendText(Environment.NewLine + "- " + url );
+    });
+                            }
+                            }
+            catch(Exception ex)
+            {
+                this.Invoke((MethodInvoker)delegate()
+                {
+                    richTextBox1.AppendText(Environment.NewLine + ex.Message + " --- " + url);
+                });
+            }
+                            worker.ReportProgress(i * (100 / totalUrl));
+                        }
+                        i++;
+
+                    }
+                }
+            
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            resultLabel.Text = (e.ProgressPercentage.ToString() + "%");
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+
+            if (e.Cancelled == true)
+            {
+                resultLabel.Text = "Canceled!";
+            }
+            else if (e.Error != null)
+            {
+                resultLabel.Text = "Error: " + e.Error.Message;
+            }
+            else
+            {
+                resultLabel.Text = "Done!";
+            }
+        }
+
+        Form2 form2;
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            form2 = new Form2(); // Вот здесь новый экземпляр создаёшь 
+            form2.Show(); 
+        }
+    }
+}
